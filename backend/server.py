2ef587bec641fa024203afb2c219ca32f13ba6ad
wthoutjc <juancamilorr2012@hotmail.com>
2021-09-17 15:05:21 -0500

    .

--- backend/server.py
+++ backend/server.py
@@ -13,15 +13,7 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 CORS(app)
 
-# MySQL CONFIG
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '9498'
-app.config['MYSQL_DB'] = 'pygroup'
-
 crud = CRUD() #Aca inicializamos nuestro CRUD
-
-app.config['SECRET_KEY'] = "#pass"
 
 #Rutas CRUD
 @app.route("/create", methods=["POST", "GET"])
